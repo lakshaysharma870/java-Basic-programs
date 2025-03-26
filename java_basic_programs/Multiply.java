7c0dd2b559772f45feed2243886d82f3fb429e78
lakshaysharma870 <lakshaysharma2399@gmail.com>
2025-03-27 00:01:56 +0530

    multiply

---
+++ java_basic_programs/Multiply.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Multiply {
+       public static void main(String[] args) {
+        int a,b,c;
+        Scanner in = new Scanner(System.in);
+
+        a=in.nextInt();
+        b=in.nextInt();
+        c=a*b;
+        System.out.println("ans :"+c);
+    }
+    
+}
